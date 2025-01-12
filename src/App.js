b8c6b6462f7c82cda44675b2f5a3ff60d45edc14
Anukul23 <141473676+Anukul23@users.noreply.github.com>
2025-01-12 17:55:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [firstName , setfirstName] = useState("");
+  const [lastName , setlastName] = useState ("");
+  const [fullName , setfullName] = useState("");
+
+  const handleformsubmit = ()=>{
+    setfullName(`Full Nmae : ${firstName} ${lastName}`)
+  }
+
+  const handlefirstName = (event)=>{
+      setfirstName(event.target.value);
+  }
+
+  const handlelastName = (event) =>{
+    setlastName(event.target.value)
+  }
+  return (
+    <div className="App">
+      <div>
+        <h1>Full Name Display</h1>
+        <form onSubmit={handleformsubmit}>
+         <label> First Name : </label>
+        <input type='text'  onChange={handlefirstName} required />
+        <br />
+        <br/>
+        <label>Last Name : </label>
+        <input type='text'  onChange={handlelastName} required />
+        <br />
+        <br/>
+        <button type='submit'>Submit</button>
+        </form>
+
+        {firstName !== "" && lastName !== "" &&(
+          <p>{fullName}</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
